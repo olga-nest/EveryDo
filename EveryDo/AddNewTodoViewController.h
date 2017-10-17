47caab78941a39ffdbe01e0fe821d5cd2956fdc3
olga-nest <olga.w.nesterova@gmail.com>
2017-10-17 16:35:36 -0700

    Add AddNewTodoViewController and add ui elements to storyboard

---
+++ EveryDo/AddNewTodoViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddNewTodoViewController.h
+//  EveryDo
+//
+//  Created by Olga on 10/17/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddNewTodoViewController : UIViewController
+
+@end
