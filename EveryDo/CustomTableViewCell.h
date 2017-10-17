0647bb20b00521eacdce2a4f56c1dca2c914da37
olga-nest <olga.w.nesterova@gmail.com>
2017-10-17 12:23:15 -0700

    Create CustomTableViewCell class

---
+++ EveryDo/CustomTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.h
+//  EveryDo
+//
+//  Created by Olga on 10/17/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableViewCell : UITableViewCell 
+
+@end
---
+++ EveryDo/CustomTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.m
+//  EveryDo
+//
+//  Created by Olga on 10/17/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import "CustomTableViewCell.h"
+
+@implementation CustomTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
